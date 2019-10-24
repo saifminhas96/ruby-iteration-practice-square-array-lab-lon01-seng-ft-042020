4af73f31be5c76cb680b2482092b95c4c887a824
github username <github email address>
2019-10-24 10:43:57 -0700

    added advanced solution with collect

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,8 @@ def square_array(array)
   array.each { |element| squared << element ** 2 }
   squared
 end
+
+# Advanced solution with .collect
+# def square_array(array)
+#   array.collect {|element| element ** 2}
+# end
